7783cd6fb910e0a11d57ec01504f21f614e8e12c
MRDenny <mrdenny@MRDennys-MacBook-Pro.local>
2019-02-13 17:25:49 -0600

    Change Project Config

--- src/main/java/com/ideaprojects/employeerest/EmployeeController.java
+++ src/main/java/com/ideaprojects/employeerest/EmployeeController.java
@@ -2,6 +2,12 @@ package com.ideaprojects.employeerest;
 
 import java.util.List;
 
+import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.Resources;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.*;
+
+
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,6 +15,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 class EmployeeController {
@@ -29,12 +36,34 @@ class EmployeeController {
         return repository.save(newEmployee);
     }
 
+    // @GetMapping("/employees/{id}")
+	// Employee one(@PathVariable Long id) {
+
+	// 	return repository.findById(id)
+	// 		.orElseThrow(() -> new EmployeeNotFoundException(id));
+    // }
+    
     @GetMapping("/employees/{id}")
-    Employee one(@PathVariable Long id){
-        
-        return repository.findById(id)
-            .orElseThrow(() -> new EmployeeNotFoundException(id));
-    }
+Resource<Employee> one(@PathVariable Long id) {
+
+	Employee employee = repository.findById(id)
+		.orElseThrow(() -> new EmployeeNotFoundException(id));
+
+	return new Resource<>(employee,
+		linkTo(methodOn(EmployeeController.class).one(id)).withSelfRel(),
+		linkTo(methodOn(EmployeeController.class).all()).withRel("employees"));
+}
+
+    // @GetMapping("/employees/{id}")
+    // Resource<Employee> one(@PathVariable Long id) {
+
+    //     Employee employee = repository.findById(id)
+    //       .orElseThrow(() -> new EmployeeNotFoundException(id));
+
+    //       return new Resource<>(employee,
+    //       linkTo(methodOn(EmployeeController.class).one(id)).withSelfRel(),
+    //       linkTo(methodOn(EmployeeController.class).all()).withRel("employees"));
+    // }
 
     @PutMapping("/employees/{id}")
     Employee replaceEmployee(@RequestBody Employee newEmployee, @PathVariable Long id){
